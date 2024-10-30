75e60136f4a2131f6b8ebf2e159a5ebbb640d401
Spimy <williamlaw.3001@gmail.com>
2024-10-31 00:15:27 +0800

    📦 NEW: Add a simple navbar layout

---
+++ app/views/layout/header.php
@@ -0,0 +1,10 @@
+<header>
+	<h1>Logo</h1>
+	<nav>
+		<ul>
+			<li><a href="<?= ROOT ?>/">Home</a></li>
+			<li><a href="<?= ROOT ?>/about">About</a></li>
+			<li><a href="<?= ROOT ?>/login">Login</a></li>
+		</ul>
+	</nav>
+</header>
