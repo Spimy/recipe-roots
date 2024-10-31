e50b7a0005a92c4463b503bda3405036d53c33c3
Spimy <williamlaw.3001@gmail.com>
2024-10-31 23:43:27 +0800

    👌 IMPROVE: Load the database and models

--- app/core/init.php
+++ app/core/init.php
@@ -1,5 +1,11 @@
 <?php
 
+spl_autoload_register( function ($classname) {
+	require $filename = "../app/models/" . ucfirst( $classname ) . ".model.php";
+} );
+
 require 'config.php';
+require 'Database.php';
+require 'Model.php';
 require 'Controller.php';
 require 'App.php';
