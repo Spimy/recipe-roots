c62cb11d236e467ca9475dcadde5eb9a5d4c42f0
Spimy <williamlaw.3001@gmail.com>
2024-10-31 23:41:32 +0800

    📦 NEW: Add database trait that creates a database on connect

---
+++ app/core/Database.php
@@ -0,0 +1,36 @@
+<?php
+
+trait Database {
+	private function connect() {
+		$connectionString = "mysql:hostname=" . DBHOST;
+
+		$connection = new PDO( $connectionString, DBUSER, DBPASS );
+		$connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+		$connection->query( "CREATE DATABASE IF NOT EXISTS `" . DBNAME . "`" );
+		$connection->query( "use " . DBNAME );
+
+		return $connection;
+	}
+
+	/**
+	 * Utility method for creating prepared statements when querying the database.
+	 * @param string $query - the SQL query with placeholders
+	 * @param array $data - the data to be inserted into the placeholders
+	 * @return array
+	 */
+	public function query( string $query, array $data = [] ) {
+		$connection = $this->connect();
+		$statement = $connection->prepare( $query );
+		$success = $statement->execute( $data );
+
+		if ( $success ) {
+			$result = $statement->fetchAll( PDO::FETCH_ASSOC );
+			if ( is_array( $result ) && count( $result ) > 0 ) {
+				return $result;
+			}
+		}
+
+		return [];
+	}
+}
