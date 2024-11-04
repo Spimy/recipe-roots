1c92418aa3755623e95a1385fdd1f9fa2b71fef0
Spimy <williamlaw.3001@gmail.com>
2024-11-04 12:23:17 +0800

    🐛 FIX: Rename login to signin

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -7,7 +7,7 @@
 		<ul role="list" class="navbar__menu__items">
 			<li><a href="<?= ROOT ?>/">Home</a></li>
 			<li><a href="<?= ROOT ?>/about">About</a></li>
-			<li><a href="<?= ROOT ?>/login">Login</a></li>
+			<li><a href="<?= ROOT ?>/signin">Sign In</a></li>
 		</ul>
 	</nav>
 </header>
