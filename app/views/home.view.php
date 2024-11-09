9257ee6574354126ea25122d64f699d6fa3361a3
Spimy <williamlaw.3001@gmail.com>
2024-11-10 01:18:39 +0800

    👌 IMPROVE: Replace accordion with details tag

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -57,39 +57,39 @@
 
 		<section class="faq">
 			<h2>FAQ</h2>
-			<div class="faq__item">
-				<input type="checkbox" id="faq1" />
-				<label for="faq1" class="faq__item__question">Are my recipes public?</label>
+
+			<details class="faq__item">
+				<summary class="faq__item__question">Are my recipes public?</summary>
 				<p class="faq__item__answer">
 					Your recipes can either be private or public depending on your
 					preference.
 				</p>
-			</div>
-			<div class="faq__item">
-				<input type="checkbox" id="faq2" />
-				<label for="faq2" class="faq__item__question">Do I need an account for each device?</label>
+			</details>
+
+			<details class="faq__item">
+				<summary class="faq__item__question">Do I need an account for each device?</summary>
 				<p class="faq__item__answer">
 					There is no need for an account for each device. As long as you log
 					into your account on a device, you'll only always need one account.
 				</p>
-			</div>
-			<div class="faq__item">
-				<input type="checkbox" id="faq3" />
-				<label for="faq3" class="faq__item__question">Can I have both a farmer and normal user account?</label>
+			</details>
+
+			<details class="faq__item">
+				<summary class="faq__item__question">Can I have both a farmer and normal user account?</summary>
 				<p class="faq__item__answer">
 					Yes, most certainly! If you produce ingredients and also have a
 					knack for creating outstanding recipes, then feel free to have access
 					to both accounts.
 				</p>
-			</div>
-			<div class="faq__item">
-				<input type="checkbox" id="faq4" />
-				<label for="faq4" class="faq__item__question">How long do ingredients take to be delivered?</label>
+			</details>
+
+			<details class="faq__item">
+				<summary class="faq__item__question">How long do ingredients take to be delivered?</summary>
 				<p class="faq__item__answer">
 					Your ingredients should be delivered to you within 1-2 hours after
 					placing your order.
 				</p>
-			</div>
+			</details>
 		</section>
 	</main>
 
