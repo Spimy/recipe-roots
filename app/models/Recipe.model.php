f2b230ed909e27427f5a398b9e7cb73b554d740b
Spimy <williamlaw.3001@gmail.com>
2024-11-18 03:20:50 +0800

    👌 IMPROVE: Add all necessary fields to recipe model

--- app/models/Recipe.model.php
+++ app/models/Recipe.model.php
@@ -2,9 +2,25 @@
 
 class Recipe extends Model {
 	protected $userId;
+	protected $title;
+	protected $thumbnail;
+	protected $prepTime;
+	protected $waitingTime;
+	protected $servings;
+	protected $public;
+	protected $ingredients;
+	protected $instructions;
 
 	public function __construct() {
 		$this->userId = $this->foreignKey( new User, true );
+		$this->title = $this->charField( 100 );
+		$this->thumbnail = $this->charField( 255, true );
+		$this->prepTime = $this->integerField( true );
+		$this->waitingTime = $this->integerField( true );
+		$this->servings = $this->integerField( true );
+		$this->public = $this->booleanField( false );
+		$this->ingredients = $this->jsonField();
+		$this->instructions = $this->textField( false );
 		parent::__construct();
 	}
 }
