e4b9e78f2cbcbeaa992e46a47bac4d0fb6a91009
Spimy <williamlaw.3001@gmail.com>
2024-11-17 19:08:43 +0800

    📦 NEW: Add utility function to check for authentication

--- app/core/utils.php
+++ app/core/utils.php
@@ -14,3 +14,13 @@ function redirect( $path ) {
 	header( "Location: " . ROOT . "/" . $path );
 	die();
 }
+
+function isAuthenticated() {
+	return isset( $_SESSION['profile'] );
+}
+
+function handleUnauthenticated( string $next ) {
+	http_response_code( 401 );
+	$_SESSION['require_auth'] = 'You need to be signed in first';
+	redirect( "signin?next=$next" );
+}
