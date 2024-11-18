4821548c572330a0a352b6a83d514756dcaeda25
Spimy <williamlaw.3001@gmail.com>
2024-11-18 17:42:29 +0800

    📦 NEW: Add method to validate inputs

--- app/models/Recipe.model.php
+++ app/models/Recipe.model.php
@@ -23,4 +23,40 @@ class Recipe extends Model {
 		$this->instructions = $this->textField( false );
 		parent::__construct();
 	}
+
+	public function validate( $data ) {
+		$errors = [];
+
+		if ( empty( $data['title'] ) ) {
+			$errors['title'] = 'Title is required';
+		}
+
+		if ( isset( $data['thumbnail'] ) ) {
+			if ( $data['thumbnail']['error'] == UPLOAD_ERR_INI_SIZE ) {
+				$errors['thumbnail'] = 'Thumbnail is too large, it should not exceed 2MB';
+			}
+		}
+
+		if ( empty( $data['public'] ) ) {
+			$errors['public'] = 'Specify whether the recipe is public or private';
+		}
+
+		if ( isset( $data['ingredients'] ) && isset( $data['units'] ) && isset( $data['amounts'] ) ) {
+			if (
+				count( $data['ingredients'] ) != count( $data['units'] )
+				|| count( $data['ingredients'] ) != count( $data['amounts'] )
+				|| count( $data['amounts'] ) != count( $data['units'] )
+			) {
+				$errors['ingredients'] = 'Ingredient details are not complete';
+			}
+		} else {
+			$errors['ingredients'] = 'At least one ingredient is required';
+		}
+
+		if ( empty( $data['instructions'] ) ) {
+			$errors['instructions'] = 'Instructions are required';
+		}
+
+		return $errors;
+	}
 }
