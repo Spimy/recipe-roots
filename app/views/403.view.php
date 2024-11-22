d6d00f21585039616be6e2ddbf09c9056352cae7
Spimy <williamlaw.3001@gmail.com>
2024-11-22 18:06:48 +0800

    📦 NEW: Add view for 403

---
+++ app/views/403.view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="<?= ROOT ?>/assets/css/styles.css">
+	<title>Recipe Roots - Forbidden</title>
+</head>
+
+<body>
+	<?php include 'layout/header.php' ?>
+
+	<main>
+		<h1>403 Forbidden</h1>
+
+		<?php if ( isset( $message ) ) : ?>
+			<p class="errors"><?= $message ?></p>
+		<?php endif; ?>
+	</main>
+
+	<?php include 'layout/footer.php' ?>
+</body>
+
+</html>
