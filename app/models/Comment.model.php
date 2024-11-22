c3a899932c060bed836111da9cb36e85f1a91fb0
Spimy <williamlaw.3001@gmail.com>
2024-11-23 00:27:11 +0800

    📦 NEW: Add comment model

---
+++ app/models/Comment.model.php
@@ -0,0 +1,33 @@
+<?php
+
+class Comment extends Model {
+	protected $profileId;
+	protected $content;
+	protected $rating;
+
+	public function __construct() {
+		$this->profileId = $this->foreignKey( new Profile(), true );
+		$this->recipeId = $this->foreignKey( new Recipe(), true );
+		$this->content = $this->textField();
+		$this->rating = $this->integerField();
+		parent::__construct();
+	}
+
+	public function validate( $data ) {
+		$errors = [];
+
+		if ( empty( $data['content'] ) ) {
+			$errors['content'] = 'Your comment has no content';
+		}
+
+		if ( empty( $data['rating'] ) ) {
+			$errors['rating'] = 'A rating is required';
+		}
+
+		if ( ! is_numeric( $data['rating'] ) ) {
+			$errors['rating'] = 'Invalid rating value';
+		}
+
+		return $errors;
+	}
+}
