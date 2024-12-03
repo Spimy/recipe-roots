fea21ba32dbd5e5abb745a8a186f565cbb09fed6
Spimy <williamlaw.3001@gmail.com>
2024-12-04 01:42:24 +0800

    📦 NEW: Add thumbnail field for ingredient

--- app/models/Ingredient.model.php
+++ app/models/Ingredient.model.php
@@ -5,12 +5,14 @@ class Ingredient extends Model {
 	protected $ingredient;
 	protected $price;
 	protected $unit;
+	protected $thumbnail;
 
 	public function __construct() {
 		$this->farmerId = $this->foreignKey( new Profile, true );
 		$this->ingredient = $this->charField( 40 );
 		$this->price = $this->decimalField( 3, 2 );
 		$this->unit = $this->charField( 4 );
+		$this->thumbnail = $this->charField( 255, true );
 		parent::__construct();
 	}
 }
