3910bffe410ab128a6e7d835128a4f39e37113e0
Spimy <williamlaw.3001@gmail.com>
2024-12-04 02:43:05 +0800

    📦 NEW: Add validation method for ingredients

--- app/models/Ingredient.model.php
+++ app/models/Ingredient.model.php
@@ -10,9 +10,40 @@ class Ingredient extends Model {
 	public function __construct() {
 		$this->farmerId = $this->foreignKey( new Profile, true );
 		$this->ingredient = $this->charField( 40 );
-		$this->price = $this->decimalField( 3, 2 );
+		$this->price = $this->decimalField( 5, 2 );
 		$this->unit = $this->charField( 4 );
 		$this->thumbnail = $this->charField( 255, true );
 		parent::__construct();
+	}
+
+	public function validate( $data ) {
+		$errors = [];
+
+		if ( empty( $data['ingredient'] ) ) {
+			$errors['ingredient'] = 'Ingredient name is required';
+		}
+
+		if ( empty( $data['price'] ) ) {
+			$errors['price'] = 'Unit price is required';
+		} else {
+			if ( ! is_numeric( $data['price'] ) ) {
+				$errors['price'] = 'Unit price should be a number';
+			} else {
+				if ( $data['price'] > 999 ) {
+					$errors['price'] = 'Unit price is too high';
+				}
+			}
+		}
+
+		if ( empty( $data['unit'] ) ) {
+			$errors['unit'] = 'Unit is required';
+		} else {
+			if ( ! in_array( $data['unit'], INGREDIENT_UNITS ) ) {
+				$errors['unit'] = 'Unit must be one of: ' . implode( ', ', INGREDIENT_UNITS );
+			}
+		}
+
+		$errors = array_merge( $errors, validateUpload( $data['thumbnail'] ) );
+		return $errors;
 	}
 }
