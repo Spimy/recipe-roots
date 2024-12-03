08e1c5db46cd6fe0ce4aae86877cf0935de4e842
Spimy <williamlaw.3001@gmail.com>
2024-12-04 03:13:59 +0800

    📦 NEW: Add controller to edit produce

--- app/controllers/Dashboard.php
+++ app/controllers/Dashboard.php
@@ -70,6 +70,10 @@ class Dashboard {
 				$this->addProduce();
 				break;
 			}
+			case 'edit': {
+				$this->editProduce( $id );
+				break;
+			}
 		}
 	}
 
@@ -100,5 +104,57 @@ class Dashboard {
 			redirect( 'dashboard' );
 		}
 		$this->view( 'farmer/produce-editor', [ 'action' => 'Add' ] );
+	}
+
+	public function editProduce( int $id ) {
+		if ( ! $id || ! is_numeric( $id ) ) {
+			http_response_code( 400 );
+			redirect( 'dashboard' );
+		}
+
+		$ingredientModel = new Ingredient();
+		$ingredient = $ingredientModel->findById( $id, true );
+
+		if ( ! $ingredient ) {
+			http_response_code( 404 );
+			return $this->view( '404' );
+		}
+
+		if ( $ingredient['farmerId'] != $this->profile['id'] ) {
+			http_response_code( 403 );
+			return $this->view( '403', [ 'message' => 'You do not have permissions to edit this produce' ] );
+		}
+
+		if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+			$errors = $ingredientModel->validate( array_merge( $_POST, $_FILES ) );
+
+			if ( count( $errors ) > 0 ) {
+				http_response_code( 400 );
+				return $this->view( 'farmer/produce-editor', [ 'action' => 'Add', 'errors' => $errors ] );
+			}
+
+			$ingredientData = [ 
+				'farmerId' => $this->profile['id'],
+				'ingredient' => $_POST['ingredient'],
+				'price' => number_format( $_POST['price'], 2, '.', '' ),
+				'unit' => $_POST['unit'],
+			];
+
+			if ( $_FILES['thumbnail']['error'] == UPLOAD_ERR_OK ) {
+				$tmp_name = $_FILES['thumbnail']['tmp_name'];
+				$name = basename( $_FILES['thumbnail']['name'] );
+				$ingredientData['thumbnail'] = uploadFile( 'thumbnails', $tmp_name, $name );
+			}
+
+			$success = $ingredientModel->update( $id, $ingredientData );
+			if ( ! $success ) {
+				http_response_code( 500 );
+				$_SESSION['recipeErrors'] = [ 'Something went wrong updating the recipe and could not be saved' ];
+			}
+
+			redirect( "dashboard" );
+		}
+
+		$this->view( 'farmer/produce-editor', [ 'action' => 'Edit', 'data' => $ingredient ] );
 	}
 }
