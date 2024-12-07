40dbb3a5f471fa3736c971dc86709bfbc6c35003
Spimy <williamlaw.3001@gmail.com>
2024-12-07 16:57:13 +0800

    👌 IMPROVE: Style list of invoices page

--- app/views/invoices/invoices.view.php
+++ app/views/invoices/invoices.view.php
@@ -5,6 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="<?= ROOT ?>/assets/css/styles.css">
+	<link rel="stylesheet" href="<?= ROOT ?>/assets/css/pages/invoices.css">
 	<title>Recipe Roots - Invoice</title>
 </head>
 
@@ -13,6 +14,37 @@
 
 	<main>
 		<h1>Invoices</h1>
+
+		<section class="invoices">
+			<?php foreach ( $invoices as $invoice ) : ?>
+				<div class="invoices__invoice">
+					<div>
+						<p><strong><?= escape( $invoice['invoiceId'] ) ?></strong></p>
+						<p><?= escape( date( 'd M Y - H:i:s', strtotime( $invoice['createdAt'] ) ) ) ?></p>
+					</div>
+					<a href="<?= ROOT ?>/ingredients/invoices/<?= escape( $invoice['invoiceId'] ) ?>"
+						class="btn btn--invert btn--next">View</a>
+				</div>
+			<?php endforeach ?>
+		</section>
+
+		<section class="paginator">
+			<div>
+				<a class="btn" href="?page=1">«</a>
+				<a class="btn" href="?page=<?= $currentPage == 1 ? $currentPage : $currentPage - 1 ?>">←</a>
+			</div>
+			<div>
+				<?php foreach ( getPaginatorPages( $currentPage, $totalPages ) as $page ) : ?>
+					<a href="?page=<?= escape( $page ) ?>" class="btn <?= $page == $currentPage ? 'selected' : '' ?>">
+						<?= escape( $page ) ?>
+					</a>
+				<?php endforeach; ?>
+			</div>
+			<div>
+				<a class="btn" href="?page=<?= $currentPage == $totalPages ? $totalPages : $currentPage + 1 ?>">→</a>
+				<a class="btn" href="?page=<?= $totalPages ?>">»</a>
+			</div>
+		</section>
 	</main>
 
 	<?php include '../app/views/layout/footer.php' ?>
