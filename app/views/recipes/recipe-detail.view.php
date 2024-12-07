7f94bdc09a9af177fd7b6eb02e40c689dfe9e7b9
Spimy <williamlaw.3001@gmail.com>
2024-12-07 17:05:35 +0800

    👌 IMPROVE: Move recipe detail css to pages folder

--- app/views/recipes/recipe-detail.view.php
+++ app/views/recipes/recipe-detail.view.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="<?= ROOT ?>/assets/css/styles.css">
-	<link rel="stylesheet" href="<?= ROOT ?>/assets/css/layout/recipe-detail.css">
+	<link rel="stylesheet" href="<?= ROOT ?>/assets/css/pages/recipe-detail.css">
 	<title>Recipe Roots - <?= escape( $recipe['title'] ) ?></title>
 </head>
 
